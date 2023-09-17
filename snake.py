57e5eb073cb4f77f992fe7c6e278510cf980ddd4
Md. Delwar Hossen <59913299+iamasik@users.noreply.github.com>
2023-09-17 14:43:49 +0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,7 +1,11 @@
+# snake.py
+
 import requests
 
+# Define your GitHub username
+username = iamasik
 
 # Fetch the GitHub contribution graph SVG
-response = requests.get(f"https://github.com/iamasik.svg")
+response = requests.get(f"https://github.com/{username}.svg")
 with open("snake.svg", "wb") as f:
     f.write(response.content)
